0d2b729636926f4bc0470f7bface599d235705e4
DESKTOP-6DNOQH9\John <johnchurchley88@gmail.com>
2017-12-06 23:14:31 +0000

    Initial Upload

---
+++ Dexmoor.BusinessHours.Core/PropertyValueConvertors/BusinessHoursConverter.cs
@@ -0,0 +1,36 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Umbraco.Core.Models.PublishedContent;
+using Umbraco.Core.PropertyEditors;
+
+namespace Dexmoor.BusinessHours.Core.PropertyValueConvertors
+{
+   public class BusinessHoursConverter
+    {
+        [PropertyValueType(typeof(IEnumerable<ViewModels.DaysOfBusinessViewModel>))]
+        [PropertyValueCache(PropertyCacheValue.All, PropertyCacheLevel.ContentCache)]
+        public class SpecialHoursOfBusinessConverter : PropertyValueConverterBase
+        {
+
+            public override bool IsConverter(PublishedPropertyType propertyType)
+            {
+                return propertyType.PropertyEditorAlias.Equals("Dexmoor.BusinessHours");
+            }
+
+            public override object ConvertSourceToObject(PublishedPropertyType propertyType, object source, bool preview)
+            {
+                if (source == null)
+                {
+                    return new List<ViewModels.DaysOfBusinessViewModel>();
+                }
+
+                var data = JsonConvert.DeserializeObject<IEnumerable<ViewModels.DaysOfBusinessViewModel>>((string)source);
+                return data;
+            }
+        }
+    }
+}
---
+++ Dexmoor.BusinessHours.Web/App_Plugins/Dexmoor.BusinessHours/directives/Dexmoor.Datepicker.directive.js
@@ -0,0 +1,24 @@
+﻿angular.module("umbraco").directive('dexmoorDatePicker', function() {
+  return {
+    restrict: 'AE',
+    require: 'ngModel',
+    scope: {
+      ngModel: '='
+    },
+    link: function(scope, element, attributes) {
+        var modelName = attributes["dexmoorDatePicker"];
+      var config = {
+        useCurrent: false,
+        format: "YYYY-MM-DD",
+        defaultDate: new Date()
+      };
+      element.datetimepicker(config);
+      element.on("dp.hide",
+        function(event) {
+          scope.ngModel = event.date.format("YYYY-MM-DD");
+          scope.$apply();
+        });
+    },
+    template: '<input name="date" type="text" ng-model="ngModel" placeholder="Select date" required/>'
+  }
+});
